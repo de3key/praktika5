6c7f78c227e871c2fe40ed23a2a9a747b92032f2
de3key <gnosthard@gmail.com>
2021-05-19 11:39:44 +0700

    Практика5

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int n = 0;
+            while (a != 0)
+            {
+                int a1 = a % 10;
+                a = a / 10;
+                if (a % 3 == 0)
+                {
+                    n = n + 1;
+                }
+            }
+
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
